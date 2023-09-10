8a7d28bfc1499cb33867d6835d3eb60938def58f
[firstname <jaffar.shaikh@1clickcapital.com>
2023-09-10 13:05:54 +0530

    added

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+ const cors = require('cors')
 
-// const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.urlencoded({extended : false}))
 app.use(express.json())
 app.use(express.static('uploads'))
 app.use("",require('./routes/route'))
+app.use(cors({ origin: "https://employee-review-system-bto7.onrender.com/", credentials: true }))
 
 mongoose.connect(process.env.DB_URI, { useNewUrlParser:true})
 
@@ -17,6 +19,6 @@ db.on('error',(error) => console.log(error))
 db.once("open", ()=>console.log("connected to database"))
 
 
-//app.listen(PORT,()=>{
-   // console.log(`Server started at http://localhost:${PORT}`) //for the checking Port
-//})
+app.listen(PORT,()=>{
+    console.log(`Server started at http://localhost:${PORT}`) //for the checking Port
+})
